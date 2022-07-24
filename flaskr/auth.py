287fcec1d36b3c4b9f9756a84e98550dc6d757fd
Joseph Ampadu <josephampadu549@gmail.com>
2022-07-24 03:02:36 +0000

    Templates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -32,7 +32,7 @@ def register():
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    "INSERT INTO users (username, password) VALUES (?, ?)",
                     (username, generate_password_hash(password)),
                 )
                 db.commit()
@@ -54,7 +54,7 @@ def login():
         db = get_db()
         error = None
         user = db.execute(
-            "SELECT * FROM user WHERE username = ?",
+            "SELECT * FROM users WHERE username = ?",
             (username,),
         ).fetchone()
 
@@ -89,7 +89,7 @@ def load_logged_in_user():
         g.user = (
             get_db()
             .execute(
-                "SELECT * FROM user WHERE id = ?",
+                "SELECT * FROM users WHERE id = ?",
                 (user_id,),
             )
             .fetchone()
